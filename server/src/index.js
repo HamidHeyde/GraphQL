47552783b4eaa5cc3634cd45fd6360fd3ccb722d
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-05-26 19:02:44 -0700

    added graphql server

--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,17 @@
-//TODO
+const { ApolloServer } = require("@apollo/server")
+const { startStandaloneServer } = require("@apollo/server/standalone")
+const typeDefs = require("./schema")
 
+async function main() {
+  const server = new ApolloServer({ typeDefs })
+  const { url } = await startStandaloneServer(server, {
+    cors: {
+      origin: "*",
+      credentials: true,
+    },
+  })
+
+  console.log(`🚀 Server ready at: ${url}`)
+}
+
+main()
