cfc105168f6aca9c7453fb923fa4ec189d14f8e7
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-05-27 14:47:33 -0700

    added query

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql } from "@apollo/client"
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+const TRACKS = gql`
+  # Query goes here
+  query GetAllTracks {
+    tracks {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`
+
 const Tracks = () => {
   return <Layout grid> </Layout>;
 };
