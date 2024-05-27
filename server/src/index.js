13a6297f8113f3c63f862a288ceb847ac1822f09
Hamid Heyde <heidarzadeh.hamidreza@gmail.com>
2024-05-27 13:07:13 -0700

    added mock data

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,17 @@
 const { ApolloServer } = require("@apollo/server")
 const { startStandaloneServer } = require("@apollo/server/standalone")
+const { addMocksToSchema } = require("@graphql-tools/mock")
+const { makeExecutableSchema } = require("@graphql-tools/schema")
 const typeDefs = require("./schema")
+const mockForSchema = require("../mockForSchema")
 
 async function main() {
-  const server = new ApolloServer({ typeDefs })
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs }),
+      mockForSchema,
+    }),
+  })
   const { url } = await startStandaloneServer(server, {
     cors: {
       origin: "*",
